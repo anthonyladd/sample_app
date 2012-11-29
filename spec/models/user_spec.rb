eef3650499689ca9c53596e296bf6d65f8c47aa8
Anthony Ladd <anthonyladd@gmail.com>
2012-11-28 19:59:28 -0600

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate)}
 
 
@@ -114,7 +115,10 @@ describe "when email format is invalid" do
   end
 
 
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 
 
